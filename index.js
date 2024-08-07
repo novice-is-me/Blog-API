24ca74a484c060b5d6d3d8e82518bea11c8febca
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-08-07 20:53:43 +0800

    Fix CORS

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.urlencoded({extended: true}));
 const corsOptions = {
 	origin: ['https://blog-website-blue-kappa.vercel.app',
 		'https://blog-website-git-master-reggys-projects-f2037f72.vercel.app',
-		'https://blog-website-k9g2nvfcq-reggys-projects-f2037f72.vercel.app'
+		'https://blog-website-3uc14i4o8-reggys-projects-f2037f72.vercel.app',
+		'http://localhost:5137'
 	],
 	credentials: true,
 	optionsSuccessStatus: 200
